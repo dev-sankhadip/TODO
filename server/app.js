fe3338e52ec7c51c4a954795b34c8f9ac9224d39
dev-sankhadip <sankhsamanta.cse17@chitkarauniversity.edu.in>
2019-11-17 17:14:24 +0530

    17 oct

---
+++ server/app.js
@@ -0,0 +1,29 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.listen(2200, function()
+{
+    console.log('runnin on 2200');
+})
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+module.exports = app;
