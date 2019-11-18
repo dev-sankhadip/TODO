598775237e4440b6c74837809c3343984983ee77
dev-sankhadip <sankhsamanta.cse17@chitkarauniversity.edu.in>
2019-11-18 13:43:52 +0530

    more features

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql'
+require('dotenv').config();
 
 const connection=mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"angular-todo"
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD ,
+    database:process.env.DB
 })
 
 export{
